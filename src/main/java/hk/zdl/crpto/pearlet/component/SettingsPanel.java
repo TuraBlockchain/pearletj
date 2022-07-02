c1ff461c5cecf6442089adfe0c9b05909261a616
David Zhang <david290@qq.com>
2022-07-03 01:25:14 +0800

    import account dialog

--- src/main/java/hk/zdl/crpto/pearlet/component/SettingsPanel.java
+++ src/main/java/hk/zdl/crpto/pearlet/component/SettingsPanel.java
@@ -2,9 +2,9 @@ package hk.zdl.crpto.pearlet.component;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -18,11 +18,13 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
@@ -39,9 +41,19 @@ import hk.zdl.crpto.pearlet.MyToolbar;
 import hk.zdl.crpto.pearlet.component.event.SettingsPanelEvent;
 import hk.zdl.crpto.pearlet.misc.AccountTableModel;
 import hk.zdl.crpto.pearlet.misc.IndepandentWindows;
+import hk.zdl.crpto.pearlet.util.CryptoUtil;
 
 @SuppressWarnings("serial")
 public class SettingsPanel extends JTabbedPane {
+
+	private static List<String> supported_networks;
+	static {
+		try {
+			supported_networks = IOUtils.readLines(SettingsPanel.class.getClassLoader().getResourceAsStream("networks.txt"), "UTF-8");
+		} catch (IOException e) {
+			supported_networks = Arrays.asList();
+		}
+	}
 
 	public SettingsPanel() {
 		addTab(SettingsPanelEvent.NET, initNetworkPanel());
@@ -51,12 +63,7 @@ public class SettingsPanel extends JTabbedPane {
 
 	private static final Component initNetworkPanel() {
 		var panel = new JPanel(new GridLayout(0, 1));
-		List<String> nws = Arrays.asList();
-		try {
-			nws = IOUtils.readLines(SettingsPanel.class.getClassLoader().getResourceAsStream("networks.txt"), "UTF-8");
-		} catch (IOException e) {
-		}
-		nws.stream().map(SettingsPanel::init_network_UI_components).forEach(panel::add);
+		supported_networks.stream().map(SettingsPanel::init_network_UI_components).forEach(panel::add);
 		var panel_1 = new JPanel(new FlowLayout());
 		panel_1.add(panel);
 		return panel_1;
@@ -89,7 +96,8 @@ public class SettingsPanel extends JTabbedPane {
 	}
 
 	private static final void createWeb3jAuthDialog(Component c) {
-		var dialog = new JDialog((Frame) SwingUtilities.getRoot(c), "Enter Project ID & Secret", true);
+		var w = SwingUtilities.getWindowAncestor(c);
+		var dialog = new JDialog(w, "Enter Project ID & Secret", Dialog.ModalityType.APPLICATION_MODAL);
 		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		IndepandentWindows.add(dialog);
 		var panel_1 = new JPanel(new GridBagLayout());
@@ -107,12 +115,13 @@ public class SettingsPanel extends JTabbedPane {
 		var btn_1 = new JButton("OK");
 		btn_1.addActionListener(e -> {
 			dialog.dispose();
+			// TODO: update data
 		});
 		panel_1.add(btn_1, new GridBagConstraints(0, 4, 2, 1, 0, 0, 10, 0, new Insets(5, 5, 10, 5), 0, 0));
 		dialog.add(panel_1);
 		dialog.pack();
 		dialog.setResizable(false);
-		dialog.setLocationByPlatform(true);
+		dialog.setLocationRelativeTo(w);
 		dialog.setVisible(true);
 	}
 
@@ -135,10 +144,73 @@ public class SettingsPanel extends JTabbedPane {
 		var del_btn = new JButton("Delete");
 		btn_panel.add(del_btn, new GridBagConstraints(0, 3, 1, 1, 0, 0, 10, 0, new Insets(5, 5, 5, 5), 0, 0));
 
+		create_account_btn.addActionListener(e -> {
+			// TODO:
+		});
+
+		import_account_btn.addActionListener(e -> create_import_account_dialog(panel));
+
+		watch_account_btn.addActionListener(e -> {
+			// TODO:
+		});
+
+		del_btn.addActionListener(e -> {
+			int i = JOptionPane.showConfirmDialog(panel.getRootPane(), "Are you sure to delete this?", "", JOptionPane.YES_NO_OPTION);
+			if (i == 0) {
+				// TODO: perform delete entry
+			}
+		});
+
 		var panel_1 = new JPanel(new FlowLayout(1, 0, 0));
 		panel_1.add(btn_panel);
 		panel.add(panel_1, BorderLayout.EAST);
 		return panel;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static final void create_import_account_dialog(Component c) {
+		var w = SwingUtilities.getWindowAncestor(c);
+		var dialog = new JDialog(w, "Import Existing Account", Dialog.ModalityType.APPLICATION_MODAL);
+		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		IndepandentWindows.add(dialog);
+		var panel = new JPanel(new GridBagLayout());
+		try {
+			panel.add(new JLabel(new MyStretchIcon(ImageIO.read(MyToolbar.class.getClassLoader().getResource("icon/" + "wallet_2.svg")), 64, 64)),
+					new GridBagConstraints(0, 0, 1, 4, 0, 0, 17, 0, new Insets(5, 5, 5, 5), 0, 0));
+		} catch (IOException e) {
+		}
+		var label_1 = new JLabel("Network:");
+		var network_combobox = new JComboBox<>();
+		network_combobox.setModel(new ListComboBoxModel<String>(supported_networks));
+		var label_2 = new JLabel("Text type:");
+		var combobox_1 = new JComboBox<>(new String[] { "Phrase" });
+		panel.add(label_1, new GridBagConstraints(1, 0, 1, 1, 0, 0, 17, 0, new Insets(5, 5, 5, 5), 0, 0));
+		panel.add(network_combobox, new GridBagConstraints(2, 0, 1, 1, 0, 0, 17, 0, new Insets(5, 5, 5, 5), 0, 0));
+		panel.add(label_2, new GridBagConstraints(3, 0, 1, 1, 0, 0, 17, 0, new Insets(5, 5, 5, 5), 0, 0));
+		panel.add(combobox_1, new GridBagConstraints(4, 0, 1, 1, 0, 0, 17, 0, new Insets(5, 5, 5, 5), 0, 0));
+		var text_area = new JTextArea(5, 30);
+		var scr_pane = new JScrollPane(text_area);
+		panel.add(scr_pane, new GridBagConstraints(1, 1, 4, 3, 0, 0, 17, 0, new Insets(5, 5, 0, 5), 0, 0));
+		var btn_1 = new JButton("OK");
+		btn_1.addActionListener(e -> {
+			String nw = network_combobox.getSelectedItem().toString();
+			String type = combobox_1.getSelectedItem().toString();
+			String text = text_area.getText().trim();
+			
+			dialog.dispose();
+			// TODO: update data
+		});
+		var panel_1 = new JPanel(new BorderLayout());
+		panel_1.add(panel, BorderLayout.CENTER);
+		var panel_2 = new JPanel(new FlowLayout());
+		panel_2.add(btn_1);
+		panel_1.add(panel_2, BorderLayout.SOUTH);
+
+		dialog.add(panel_1);
+		dialog.pack();
+		dialog.setResizable(false);
+		dialog.setLocationRelativeTo(w);
+		dialog.setVisible(true);
 	}
 
 	@Subscribe(threadMode = ThreadMode.ASYNC)
