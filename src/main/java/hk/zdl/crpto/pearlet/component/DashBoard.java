3186822c896cfce33b9fcccdf460836c2078905f
David Zhang <david290@qq.com>
2022-07-05 16:19:37 +0800

    add modal animation to dashboard

--- src/main/java/hk/zdl/crpto/pearlet/component/DashBoard.java
+++ src/main/java/hk/zdl/crpto/pearlet/component/DashBoard.java
@@ -6,12 +6,14 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JLayer;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -29,6 +31,7 @@ import org.greenrobot.eventbus.ThreadMode;
 import hk.zdl.crpto.pearlet.component.dashboard.DashboardTxProc;
 import hk.zdl.crpto.pearlet.component.dashboard.DashboardTxTableModel;
 import hk.zdl.crpto.pearlet.component.event.AccountChangeEvent;
+import hk.zdl.crpto.pearlet.ui.WaitLayerUI;
 import hk.zdl.crpto.pearlet.util.CryptoUtil;
 import hk.zdl.crpto.pearlet.util.Util;
 
@@ -36,6 +39,8 @@ import hk.zdl.crpto.pearlet.util.Util;
 public class DashBoard extends JPanel {
 
 	private static Font title_font = new Font("Arial Black", Font.PLAIN, 16);
+	private final JLayer<JPanel> jlayer = new JLayer<>();
+	private final WaitLayerUI wlui = new WaitLayerUI();
 	private final JPanel token_list_inner_panel = new JPanel(new GridLayout(0, 1));
 	private final JPanel token_list_panel = new JPanel(new BorderLayout());
 	private final JLabel currency_label = new JLabel(), balance_label = new JLabel();
@@ -47,9 +52,13 @@ public class DashBoard extends JPanel {
 	public DashBoard() {
 		super(new BorderLayout());
 		EventBus.getDefault().register(this);
+		add(jlayer, BorderLayout.CENTER);
+		var _panel = new JPanel(new BorderLayout());
+		jlayer.setView(_panel);
+		jlayer.setUI(wlui);
 		var balance_and_tx_panel = new JPanel(new BorderLayout());
-		add(token_list_panel, BorderLayout.WEST);
-		add(balance_and_tx_panel, BorderLayout.CENTER);
+		_panel.add(token_list_panel, BorderLayout.WEST);
+		_panel.add(balance_and_tx_panel, BorderLayout.CENTER);
 		token_list_panel.setMinimumSize(new Dimension(200, 0));
 		var label1 = new JLabel("Tokens:");
 		var panel0 = new JPanel(new FlowLayout(0));
@@ -84,6 +93,7 @@ public class DashBoard extends JPanel {
 		table.setShowGrid(true);
 		adjust_table_width();
 		balance_and_tx_panel.add(scrollpane, BorderLayout.CENTER);
+
 	}
 
 	@Subscribe(threadMode = ThreadMode.ASYNC)
@@ -94,15 +104,20 @@ public class DashBoard extends JPanel {
 		if (address.equals("null")) {
 			balance_label.setText("0");
 		} else {
+			wlui.start();
 			balance_label.setText("?");
 			Util.submit(() -> {
 				try {
+					table_model.setData(Arrays.asList());
+					SwingUtilities.invokeLater(() -> adjust_table_width());
 					dbtp.update_column_model(e.network, table_column_model, address);
 					dbtp.update_data(e.network, address);
-					SwingUtilities.invokeLater(()->adjust_table_width());
+					SwingUtilities.invokeLater(() -> adjust_table_width());
 					table.updateUI();
 				} catch (Exception x) {
 					Logger.getLogger(getClass().getName()).log(Level.SEVERE, x.getMessage(), x);
+				} finally {
+					wlui.stop();
 				}
 
 			});
@@ -117,7 +132,6 @@ public class DashBoard extends JPanel {
 	}
 
 	private void adjust_table_width() {
-		int total_width = 0;
 		for (int column = 0; column < table.getColumnCount(); column++) {
 			int width = 100; // Min width
 			for (int row = 0; row < table.getRowCount(); row++) {
@@ -129,9 +143,7 @@ public class DashBoard extends JPanel {
 				width = 300;
 			table_column_model.getColumn(column).setMinWidth(width);
 			table_column_model.getColumn(column).setPreferredWidth(width);
-			total_width +=width;
 		}
-		table.setPreferredScrollableViewportSize(new Dimension(total_width,500));
 	}
 
 }
