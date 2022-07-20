f5f8fdb251832b2568f5b8e407de9d11cf2c544c
David Zhang <david290@qq.com>
2022-07-20 13:13:32 +0800

    impl miner plot file path configure

--- src/main/java/hk/zdl/crypto/pearlet/component/miner/MinerSettingsPane.java
+++ src/main/java/hk/zdl/crypto/pearlet/component/miner/MinerSettingsPane.java
@@ -14,6 +14,7 @@ public class MinerSettingsPane extends JPanel {
 	private static final long serialVersionUID = -3040618948690123951L;
 	private final MinerAccountSettingsPanel account_settings_panel = new MinerAccountSettingsPanel();
 	private final MinerMiscSettingsPane misc_settings_panel = new MinerMiscSettingsPane();
+	private final MinerPathSettingPanel miner_path_settings_panel = new MinerPathSettingPanel();
 	
 	public MinerSettingsPane() {
 		super(new GridLayout(2, 2));
@@ -23,6 +24,7 @@ public class MinerSettingsPane extends JPanel {
 	private void init_panels() {
 		add(account_settings_panel);
 		add(misc_settings_panel);
+		add(miner_path_settings_panel);
 	}
 
 	public void setBasePath(String basePath) {
@@ -35,6 +37,12 @@ public class MinerSettingsPane extends JPanel {
 		misc_settings_panel.setBasePath(basePath);
 		try {
 			misc_settings_panel.update_server_address();
+		} catch (Exception e) {
+			Logger.getLogger(getClass().getName()).log(Level.WARNING, e.getMessage(), e);
+		}
+		miner_path_settings_panel.setBasePath(basePath);
+		try {
+			miner_path_settings_panel.refresh_list();
 		} catch (Exception e) {
 			Logger.getLogger(getClass().getName()).log(Level.WARNING, e.getMessage(), e);
 		}
