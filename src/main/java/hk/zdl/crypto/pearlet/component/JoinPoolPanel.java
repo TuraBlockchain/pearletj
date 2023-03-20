10b785ab27e18977d05c180841aea7e43645bf28
David Zhang <david290@qq.com>
2023-03-20 11:14:19 +0800

    add limit on Join Pool on PETH

--- src/main/java/hk/zdl/crypto/pearlet/component/JoinPoolPanel.java
+++ src/main/java/hk/zdl/crypto/pearlet/component/JoinPoolPanel.java
@@ -37,15 +37,15 @@ public class JoinPoolPanel extends JPanel implements ActionListener {
 	private static final String NONE = "None";
 	private static final long serialVersionUID = -8477305312112985648L;
 	private final JProgressBar bar = new JProgressBar();
+	private final JButton btn = new JButton("Change");
 	private CryptoNetwork network;
 	private String account;
 
 	public JoinPoolPanel() {
 		super(new GridBagLayout());
 		EventBus.getDefault().register(this);
-		add(new JLabel("Pool Address:"), new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
+		add(new JLabel("Reward Recipient:"), new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
 		add(bar, new GridBagConstraints(1, 0, 1, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
-		var btn = new JButton("Change");
 		add(btn, new GridBagConstraints(2, 0, 1, 1, 0, 0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
 		btn.addActionListener(this);
 		bar.setStringPainted(true);
@@ -65,6 +65,9 @@ public class JoinPoolPanel extends JPanel implements ActionListener {
 			} catch (Exception x) {
 				Logger.getLogger(getClass().getName()).log(Level.WARNING, x.getMessage(), x);
 			}
+			btn.setEnabled(true);
+		}else {
+			btn.setEnabled(false);
 		}
 	}
 
@@ -74,7 +77,7 @@ public class JoinPoolPanel extends JPanel implements ActionListener {
 		var txt_field = new JTextField(30);
 		var a = new JLabel();
 		a.setHorizontalAlignment(SwingConstants.RIGHT);
-		panel.add(new JLabel("Please specify pool address:"), new GridBagConstraints(0, 0, 2, 1, 2, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+		panel.add(new JLabel("Please specify reward recipient:"), new GridBagConstraints(0, 0, 2, 1, 2, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
 		panel.add(txt_field, new GridBagConstraints(0, 1, 2, 1, 2, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
 		panel.add(new JLabel("Tx fee:"), new GridBagConstraints(0, 2, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
 		panel.add(a, new GridBagConstraints(1, 2, 1, 1, 0, 0, GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
@@ -85,8 +88,13 @@ public class JoinPoolPanel extends JPanel implements ActionListener {
 			a.setText(txt);
 			return null;
 		});
-		int i = JOptionPane.showConfirmDialog(getRootPane(), panel, "Pool Address", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+		int i = JOptionPane.showConfirmDialog(getRootPane(), panel, "Reward Recipient", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
 		if (i == JOptionPane.OK_OPTION) {
+			var target = txt_field.getText();
+			if(target.isBlank()) {
+				JOptionPane.showMessageDialog(getRootPane(), "Reward recipient cannot be empty!", "ERROR", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
 			Util.submit(() -> {
 				try {
 					bar.setIndeterminate(true);
@@ -108,7 +116,8 @@ public class JoinPoolPanel extends JPanel implements ActionListener {
 					var fee_qnt = CryptoUtil.getFeeSuggestion(network).getCheapFee().toNQT();
 					var decimalPlaces = CryptoUtil.getConstants(network).getInt("decimalPlaces");
 					var fee_dml = new BigDecimal(fee_qnt, decimalPlaces);
-					byte[] unsigned = CryptoUtil.setRewardRecipient(network, account, public_key, txt_field.getText(), fee_dml);
+					check_permission();
+					byte[] unsigned = CryptoUtil.setRewardRecipient(network, account, public_key, target, fee_dml);
 					byte[] signed = CryptoUtil.signTransaction(network, private_key, unsigned);
 					CryptoUtil.broadcastTransaction(network, signed);
 					UIUtil.displayMessage("Join Pool", "Join pool complete.");
@@ -119,6 +128,17 @@ public class JoinPoolPanel extends JPanel implements ActionListener {
 					((JButton) e.getSource()).setEnabled(true);
 				}
 			});
+		}
+	}
+
+	private void check_permission() throws Exception {
+		if (network.isBurst() && network.getUrl().toLowerCase().contains("mainnet.peth.world")) {
+			var committed_balance = CryptoUtil.getAccount(network, this.account).getCommittedBalance();
+			var decimalPlaces = CryptoUtil.getConstants(network).getInt("decimalPlaces");
+			var _c_bal = new BigDecimal(committed_balance.toNQT(), decimalPlaces);
+			if (_c_bal.compareTo(new BigDecimal("10000")) < 0) {
+				throw new IllegalStateException("Insufficient Commitment!");
+			}
 		}
 	}
 
