07ecb8b3c23f1887dc2a73c013fadf6077dbba2b
David Zhang <david290@qq.com>
2023-05-24 06:09:25 +0800

    add miner support via docker

--- src/main/java/hk/zdl/crypto/pearlet/component/miner/local/LocalMiner.java
+++ src/main/java/hk/zdl/crypto/pearlet/component/miner/local/LocalMiner.java
@@ -1,14 +1,20 @@
 package hk.zdl.crypto.pearlet.component.miner.local;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Stream;
 import java.util.zip.ZipFile;
@@ -56,11 +62,11 @@ public class LocalMiner {
 			} catch (IOException e) {
 				return Stream.empty();
 			}
-		}).filter(p->p.toFile().getName().startsWith(id+"_")&&Files.isRegularFile(p)).mapToLong(value -> {
+		}).filter(p -> p.toFile().getName().startsWith(id + "_") && Files.isRegularFile(p)).mapToLong(value -> {
 			try {
 				return Files.size(value);
 			} catch (IOException e) {
-			return 0;
+				return 0;
 			}
 		}).sum();
 		m.put("additional_headers", Collections.singletonMap("X-Filesize", fz));
@@ -70,8 +76,31 @@ public class LocalMiner {
 		return conf_file;
 	}
 
+	@SuppressWarnings("unchecked")
 	public static Process build_process(File miner_bin, File conf_file) throws Exception {
-		return new ProcessBuilder(miner_bin.getAbsolutePath(), "-c", conf_file.getAbsolutePath()).directory(tmp_dir).start();
+		if (SystemInfo.isAARCH64) {
+			var proc = new ProcessBuilder("docker", "run", "--privileged", "--rm", "tonistiigi/binfmt", "--install", "linux/amd64").start();
+			var i = proc.waitFor();
+			if (i != 0) {
+				var err_info = IOUtils.readLines(proc.getErrorStream(), Charset.defaultCharset()).stream().reduce("", (a, b) -> a + "\n" + b).trim();
+				throw new IOException(err_info);
+			} else {
+				var l = new LinkedList<String>();
+				l.addAll(Arrays.asList("docker", "run", "--platform", "linux/amd64", "--mount", "type=bind,source=" + miner_bin.getAbsolutePath() + ",target=/app/signum-miner"));
+				var x = new LinkedList<String>();
+				x.add(conf_file.getAbsolutePath());
+				x.addAll((List<String>)new Yaml().loadAs(new FileInputStream(conf_file), Map.class).get("plot_dirs"));
+				x.forEach(s -> {
+					l.addAll(Arrays.asList("--mount", "type=bind,source=" + s + ",target=" + s));
+				});
+				l.addAll(Arrays.asList("ubuntu", "/app/signum-miner", "-c", conf_file.getAbsolutePath()));
+				proc = new ProcessBuilder(l).start();
+			}
+			return proc;
+		} else {
+			return new ProcessBuilder(miner_bin.getAbsolutePath(), "-c", conf_file.getAbsolutePath()).directory(tmp_dir).start();
+		}
+
 	}
 
 	public static File copy_miner() throws IOException {
