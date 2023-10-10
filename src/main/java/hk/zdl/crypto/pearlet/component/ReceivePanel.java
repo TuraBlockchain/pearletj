40a66cf52133e09dda41a9339eae28205aa98c88
David Zhang <david290@qq.com>
2023-10-10 13:45:08 +0800

    change white bg in qrcode into opaque

--- src/main/java/hk/zdl/crypto/pearlet/component/ReceivePanel.java
+++ src/main/java/hk/zdl/crypto/pearlet/component/ReceivePanel.java
@@ -1,6 +1,7 @@
 package hk.zdl.crypto.pearlet.component;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -9,6 +10,8 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.awt.image.BufferedImage;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.RGBImageFilter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
@@ -80,7 +83,7 @@ public class ReceivePanel extends JPanel {
 	@Subscribe(threadMode = ThreadMode.ASYNC)
 	public void onMessage(AccountChangeEvent e) {
 		setText(e.account);
-		BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_3BYTE_BGR);
+		Image img = new BufferedImage(1, 1, BufferedImage.TYPE_3BYTE_BGR);
 		try {
 			String qr_str = "";
 			if (e.network.isBurst()) {
@@ -91,11 +94,32 @@ public class ReceivePanel extends JPanel {
 			} else if (e.network.isWeb3J()) {
 				qr_str = "ethereum:" + e.account;
 			}
-			ByteArrayOutputStream baos = new ByteArrayOutputStream(5120);
+			var baos = new ByteArrayOutputStream(5120);
 			QRCode.from(qr_str).to(ImageType.GIF).writeTo(baos);
 			img = ImageIO.read(new ByteArrayInputStream(baos.toByteArray()));
+			img = makeColorTransparent(img, Color.WHITE);
 		} catch (Exception x) {
 		}
 		qr_code.setIcon(new ImageIcon(img));
+	}
+
+	public static Image makeColorTransparent(Image im, final Color color) {
+		var filter = new RGBImageFilter() {
+			// the color we are looking for... Alpha bits are set to opaque
+			public int markerRGB = color.getRGB() | 0xFF000000;
+
+			public final int filterRGB(int x, int y, int rgb) {
+				if ((rgb | 0xFF000000) == markerRGB) {
+					// Mark the alpha bits as zero - transparent
+					return 0x00FFFFFF & rgb;
+				} else {
+					// nothing to do
+					return rgb;
+				}
+			}
+		};
+
+		var ip = new FilteredImageSource(im.getSource(), filter);
+		return Toolkit.getDefaultToolkit().createImage(ip);
 	}
 }
